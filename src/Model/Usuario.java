92a22aace293cbb19dd5eefe1dc8e4993bed2921
Jeremi González Perera <gonzalezjeremi532@gmail.com>
2026-03-09 10:51:51 +0000

    Subida de archivos Usuario y nota .java

---
+++ src/Model/Usuario.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class Usuario {
+    private String username;
+    private String passwordHash;
+
+    public Usuario(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+}
